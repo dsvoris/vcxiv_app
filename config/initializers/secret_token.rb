7e7fc4a5e6aa27b55f82d85241e38aa368c23e0c
dsvoris <dsvoris@gmail.com>
2013-07-29 22:56:57 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VcxivApp::Application.config.secret_key_base = '8138667bcd7086e27be48d9ef97d60c495e59381213617750f782cdc21d668e70a9de94a7ec4c194f8fdf3cadd51978de351d98b13973005d43d9f0074faa85d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VcxivApp::Application.config.session_store :cookie_store, key: '_vcxiv_app_session'
